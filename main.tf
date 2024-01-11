410440db76160880ec084050e519d16b5da6f29b
balusena <balusena12@gmail.com>
2024-01-11 15:00:50 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_nat_gateway" "ngw" {
   var.tags)
 }
 
-resource "aws_nat_gateway" "route-ngw" {
+resource "aws_route" "route-ngw" {
   count                  = length(local.private_route_table_ids)
   route_table_id         = element(local.private_route_table_ids, count.index)
   destination_cidr_block = "0.0.0.0/0"
