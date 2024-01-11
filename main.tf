438cd48608502bbf2e1d4fe91cd4c4e6b01a4eee
balusena <balusena12@gmail.com>
2024-01-11 13:50:17 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -41,3 +41,17 @@ resource "aws_route" "route-igw" {
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id                = aws_internet_gateway.igw.id
 }
+
+resource "aws_eip" "ngw" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)[0]
+
+  tags = merge({
+    Name = "${var.env}-ngw"
+  },
+  var.tags)
+}
