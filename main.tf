b24fa0e35822dea776bee9f09ff08815b6ec379e
balusena <balusena12@gmail.com>
2024-01-11 12:55:38 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -33,17 +33,11 @@ resource "aws_internet_gateway" "igw" {
   tags = merge({
     Name = "${var.env}-igw"
   },
-    var.tags)
+  var.tags)
 }
 
 resource "aws_route" "route-igw" {
-  route_table_id         = module.subnets.public_route_table_ids[0]
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  route_table_id            = module.subnets["public"].route_table_ids
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.igw.id
 }
-
-#resource "aws_route" "route-igw" {
-#  route_table_id            = module.subnets["public"].route_table_ids
-#  destination_cidr_block    = "0.0.0.0/0"
-#  gateway_id                = aws_internet_gateway.igw.id
-#}
--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -2,5 +2,11 @@ output "subnet_ids" {
   value = aws_subnet.main.*.id
 }
 
+output "route_table_ids" {
+  value = aws_route_table.table.id
+}
+
+
+
 
 
